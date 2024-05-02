041b02f3507e21fbc421a5eafc654084ce926afd
Kroojel <joesexton123@outlook.com>
2024-05-03 00:34:53 +0100

    Renamed and reworked

--- src/client/java/com/holebois/client_maps/client_mapsClient.java
+++ src/client/java/com/holebois/client_maps/client_mapsClient.java
@@ -1,18 +1,21 @@
-package com.holebois.localmaps;
+package com.holebois.client_maps;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.item.FilledMapItem;
+import net.minecraft.world.World;
 
-public class localmapsClient implements ClientModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("localmaps");
+public class client_mapsClient implements ClientModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("client_maps");
 	public static MinecraftClient client;
 
 	@Override
@@ -22,17 +25,17 @@ public class localmapsClient implements ClientModInitializer {
 
 	public static byte[] getMap(Integer mapId) {
         client = MinecraftClient.getInstance();
-        File save_dir = new File(client.runDirectory, ".localmaps");
         if (client.isInSingleplayer()) {
             return null;
         }
+        File save_dir = new File(client.runDirectory, ".client_maps");
         save_dir = new File(save_dir, client.getCurrentServerEntry().address);
         File mapfile = new File(save_dir, String.valueOf(mapId));
         byte[] data = new byte[(int) mapfile.length()];
         try (FileInputStream stream = new FileInputStream(mapfile)) {
             stream.read(data);
         } catch (IOException e) {
-            LOGGER.error("Could not read map file " + mapfile.getAbsolutePath() + " cannot continue!");
+            // LOGGER.error("Could not read map file " + mapfile.getAbsolutePath() + " cannot continue!");
             return null;
         }
         return data;
@@ -40,10 +43,12 @@ public class localmapsClient implements ClientModInitializer {
 
 	public static void setMap(Integer mapId, byte[] data) throws FileNotFoundException, IOException, ClassNotFoundException {
         client = MinecraftClient.getInstance();
-		File save_dir = new File(client.runDirectory, ".localmaps");
-        if (client.isInSingleplayer()) {
+        World world = client.world;
+        if (client.isInSingleplayer() || data == null || Arrays.equals(data, world.getMapState(FilledMapItem.getMapName(mapId)).colors)) {
+            System.out.println("Returning");
             return;
         }
+		File save_dir = new File(client.runDirectory, ".client_maps");
 		save_dir = new File(save_dir, client.getCurrentServerEntry().address);
 
 
--- src/client/java/com/holebois/client_maps/mixin/client/client_mapsClientMixin.java
+++ src/client/java/com/holebois/client_maps/mixin/client/client_mapsClientMixin.java
@@ -1,11 +1,11 @@
-package com.holebois.localmaps.mixin.client;
+package com.holebois.client_maps.mixin.client;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import com.holebois.localmaps.localmapsClient;
+import com.holebois.client_maps.client_mapsClient;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.item.FilledMapItem;
@@ -13,20 +13,20 @@ import net.minecraft.item.map.MapState;
 import net.minecraft.world.World;
 
 @Mixin(FilledMapItem.class)
-public class localmapsClientMixin {
+public class client_mapsClientMixin {
 	@Inject(at = @At("RETURN"), method = "getMapState", cancellable = true)
 	private static void getMapState(Integer id, World world, CallbackInfoReturnable<MapState> cir) {
 		if (MinecraftClient.getInstance().isInSingleplayer()) return;
 		MapState state = cir.getReturnValue();
 		if (state != null) {
 			try {
-				localmapsClient.setMap(id, state.colors);
+				client_mapsClient.setMap(id, state.colors);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		} else {
 			state = MapState.of(0, 0, (byte)1, false, false, null);
-			byte[] colors = localmapsClient.getMap(id);
+			byte[] colors = client_mapsClient.getMap(id);
 			if (colors == null) return;
 			state.colors = colors;
 			cir.setReturnValue(state);
