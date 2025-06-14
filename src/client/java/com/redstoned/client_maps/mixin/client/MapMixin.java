bfd3a7f37010fe35245a1c739df13362cca511d1
galiano <piz@redstoned.dev>
2025-06-14 00:04:29 -0500

    async load & desync fix

--- src/client/java/com/redstoned/client_maps/mixin/client/MapMixin.java
+++ src/client/java/com/redstoned/client_maps/mixin/client/MapMixin.java
@@ -1,13 +1,13 @@
 package com.redstoned.client_maps.mixin.client;
 
+import net.minecraft.util.Util;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import com.redstoned.client_maps.client_mapsClient;
+import com.redstoned.client_maps.ClientMaps;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.component.type.MapIdComponent;
 import net.minecraft.item.FilledMapItem;
@@ -15,32 +15,41 @@ import net.minecraft.item.map.MapState;
 import net.minecraft.world.World;
 
 @Mixin(FilledMapItem.class)
-public class client_mapsClientMixin {
+public class MapMixin {
 	@Inject(at = @At("RETURN"), method = "getMapState", cancellable = true)
 	private static void getMapState(MapIdComponent id, World world, CallbackInfoReturnable<MapState> cir) {
 		MapState state = cir.getReturnValue();
 
 		Integer mapId = id != null ? id.id() : null;
+		if (mapId == null) return;
+		if (ClientMaps.pending.contains(mapId)) return;
 
-		if (state != null && mapId != null) {
+		if (state != null) {
 			try {
-				client_mapsClient.setMap(mapId, state.colors);
+				ClientMaps.setMap(mapId, state.colors);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		} else {
-			// If the map state does not exist, create a dummy one
-			if (state == null) {
-				state = MapState.of(0, 0, (byte)1, false, false, null);
-			}
-			byte[] colors = client_mapsClient.getMap(mapId);
-			if (colors == null) return;
-			state.colors = colors;
+			// register this id as pending load from disk
+			ClientMaps.pending.add(mapId);
 
-			ClientWorld clientWorld = (ClientWorld) world;
-			clientWorld.putClientsideMapState(id, state);
+			Util.getIoWorkerExecutor().execute(() -> {
+				// The map state does not exist, create a dummy one
+				MapState dummyState = MapState.of(0, 0, ClientMaps.MARKER, false, false, null);
+				byte[] colors = ClientMaps.getMap(mapId);
+				if (colors == null) {
+					ClientMaps.pending.remove(mapId);
+					return;
+				};
+				dummyState.colors = colors;
 
-			cir.setReturnValue(state);
+				ClientWorld clientWorld = (ClientWorld) world;
+				clientWorld.putClientsideMapState(id, dummyState);
+				ClientMaps.pending.remove(mapId);
+			});
+
+//			cir.setReturnValue(state);
 		}
 	}
 }
